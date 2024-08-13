d71252f08d1e83ad184f75a1c9c64d48d44168f5
YvesCB <y.bruckmayer@gmail.com>
2024-08-13 16:57:49 +0200

    started work on overhaul

---
+++ front/tokengrabber/src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<nav>
+  <a href="/">Home</a>
+  <a href="/">Infopanel</a>
+  <a href="/">Test</a>
+</nav>
+
+<slot />
+
+<style>
+  nav {
+    display: flex;
+    gap: var(--size-7);
+  }
+</style>
--- front/tokengrabber/src/routes/+page.svelte
+++ front/tokengrabber/src/routes/+page.svelte
@@ -1,15 +1,3 @@
-<script lang="ts">
-  import SsoRedirectButton from "$lib/SSORedirectButton.svelte";
-</script>
-
 <main>
   <h1>Welcome</h1>
-  <SsoRedirectButton />
 </main>
-
-<style>
-  main {
-    max-width: 80rem;
-    line-break: anywhere;
-  }
-</style>
